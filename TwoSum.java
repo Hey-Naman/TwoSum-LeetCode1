300c751930450e6f80cec89841d7e790e70124cf
Hey-Naman <namancollege1408@gmail.com>
2024-01-01 04:07:30 +0530

    TwoSum

--- TwoSum.java
+++ TwoSum.java
@@ -5,19 +5,19 @@ public class TwoSum{
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the size of array: "); //User provide the array size
         int arraySize = scanner.nextInt();
-        int[] array = new int[arraySize]; //initilizing array
+        int[] array = new int[arraySize];               //initilizing array
         for(int i=0; i<arraySize;i++){
             System.out.print("Enter your "+i+" element of array: "); //User provinding array
             array[i] = scanner.nextInt();
         }
-        System.out.print("You want the sum of: "); // Target, the sum of two
+        System.out.print("You want the sum of: ");    // Target, the sum of two
         int target = scanner.nextInt();
-        boolean isFound = false; //used to check if we found the sum or not
-        for(int i=0;i<arraySize-1;i++){
-            for(int j=i+1;j<arraySize;j++){
+        boolean isFound = false;                        //used to check if we found the sum or not
+        for(int i=0;i<arraySize-1;i++){                 // outer loop to check the first Number
+            for(int j=i+1;j<arraySize;j++){             // inner loop to check the second number
                 if(target == array[i]+array[i+1]){
                     System.out.println("The number is found on the index "+ i+" and "+j); // if the sum is found
-                    isFound = true;
+                    isFound = true;                     //boolean returning true if we found the number
                     return;
                 }
             }
